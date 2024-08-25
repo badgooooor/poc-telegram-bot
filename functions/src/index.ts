f7442d016fb2b5701f99bb33ba4754e108df1de8
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2024-08-25 17:09:07 +0700

    :sparkles: Add some command and handlings

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,27 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { logger } from "firebase-functions/v2";
+import { onRequest } from "firebase-functions/v2/https";
+import { Telegraf } from "telegraf";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN as string;
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
+// Handling command
+bot.command('/start', ctx => 
+  ctx.reply('Hello! Send any message and I will copy it.')
+);
+
+// Send handling messages.
+bot.on('message', (ctx) => ctx.reply('Got message'));
+
+export const telegramBot = onRequest(async (request, response) => {
+  logger.log('message sent with body', request.body, request.headers);
+
+  await bot.handleUpdate(request.body, response).then(() => {
+		// if it's not a request from the telegram, rv will be undefined, but we should respond with 200
+		return response.sendStatus(200);
+	});
+});
